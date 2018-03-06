0b4773eb6683a3187b703af4b368b70db7da7542
Aaron Gooden <aagooden@me.com>
2018-03-06 13:38:07 -0500

    First test returned array passed

--- test_encoding_message.rb
+++ test_encoding_message.rb
@@ -1,9 +1,21 @@
 require "minitest/autorun"
 require_relative "encoding_message.rb"
 
-class Addition_test < Minitest::Test
+class Encoding_test < Minitest::Test
 
 	def test_boolean
 		assert_equal(true, true)
 	end
+
+	def test_give_string_gives_string
+		message = "I am a message"
+		assert_equal(String, encode(message).class)
+	end
+
+	def test_return_array
+		message = "I am a message"
+		assert_equal(Array, convert(message).class)
+	end
+
+
 end
